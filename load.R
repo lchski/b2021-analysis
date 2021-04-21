9ae6dcb2b0d59657a095e028606f7ae48ad7f74a
Lucas Cherkewski <lucas.cherkewski@cds-snc.ca>
2021-04-21 17:17:47 -0400

    Extract proposals

--- load.R
+++ load.R
@@ -3,14 +3,21 @@ library(rvest)
 
 source("lib/retrieve-pages.R")
 
-budget_doc_urls <- c(
-  "https://www.budget.gc.ca/2021/report-rapport/p1-en.html",
-  "https://www.budget.gc.ca/2021/report-rapport/p2-en.html",
-  "https://www.budget.gc.ca/2021/report-rapport/p3-en.html",
-  "https://www.budget.gc.ca/2021/report-rapport/p4-en.html"
-)
-
-budget_docs <- tibble(url = budget_doc_urls) %>%
+budget_docs <- tribble(
+    ~part, ~title, ~url,
+    1, "Finishing the Fight Against COVID-19", "https://www.budget.gc.ca/2021/report-rapport/p1-en.html",
+    2, "Creating Jobs and Growth", "https://www.budget.gc.ca/2021/report-rapport/p2-en.html",
+    3, "A Resilient and Inclusive Recovery", "https://www.budget.gc.ca/2021/report-rapport/p3-en.html",
+    4, "Fair and Responsible Government", "https://www.budget.gc.ca/2021/report-rapport/p4-en.html"
+  ) %>%
   mutate(page = map(url, retrieve_page_at_url))
 
-
+proposals <- budget_docs %>%
+  mutate(proposal = map(
+    page,
+    ~ .x %>%
+      html_elements(".media-body") %>%
+      html_text2()
+  )) %>%
+  unnest(cols = c(proposal)) %>%
+  select(part, proposal)
